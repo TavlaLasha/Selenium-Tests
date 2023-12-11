6cc2aed8fa47198be803b2940b8836475ff6395f
TavlaLasha <55584483+TavlaLasha@users.noreply.github.com>
2023-12-11 22:17:32 +0400

    Some changes

--- SeleniumTestProject/src/main/java/pages/PracticeFormPage.java
+++ SeleniumTestProject/src/main/java/pages/PracticeFormPage.java
@@ -13,22 +13,26 @@ public class PracticeFormPage {
     }
 
     public static WebElement TextBox_LastName(WebDriver driver){
-        element = driver.findElement(By.xpath("//*[@id=\"lastName\"]"));
+        element = driver.findElement(By.xpath("//input[@id=\"lastName\"]"));
         return element;
     }
 
     public static WebElement radio_gender(WebDriver driver){
-        element = driver.findElement(By.xpath("//*[@for=\"gender-radio-1\"]"));
+        element = driver.findElement(By.xpath("//label[@for=\"gender-radio-1\"]"));
         return element;
     }
 
     public static WebElement TextBox_Mobile(WebDriver driver){
-        element = driver.findElement(By.xpath("//*[@id=\"userNumber\"]"));
+        element = driver.findElement(By.xpath("//input[@id=\"userNumber\"]"));
         return element;
     }
 
     public static WebElement Button_Submit(WebDriver driver){
         element = driver.findElement(By.xpath("//button[@id=\"submit\"]"));
         return element;
+    }
+
+    public static By Dialog_Success_Text_Locator(){
+        return By.xpath("//div[(@id=\"example-modal-sizes-title-lg\") and (text()=\"Thanks for submitting the form\")]");
     }
 }
--- SeleniumTestProject/src/test/java/POM_Allure.java
+++ SeleniumTestProject/src/test/java/POM_Allure.java
@@ -2,19 +2,26 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
 
 import org.testng.annotations.Test;
 import pages.*;
+
+import java.time.Duration;
 
 public class POM_Allure {
     WebDriver driver;
 
     @BeforeTest
     @Severity(SeverityLevel.CRITICAL)
-    public void start() {
+    public void BrowserInitialization() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://demoqa.com/");
@@ -23,13 +30,16 @@ public class POM_Allure {
 
     @Test(priority = 0)
     @Severity(SeverityLevel.BLOCKER)
+    @Step("Go to form page")
+    @Description("Go to form page")
     private void GoToForm(){
         HomePage.FormsButton(driver).click();
         FormsPage.PracticeFormOptionBtn(driver).click();
     }
 
     @Test(priority = 1)
-    @Severity(SeverityLevel.NORMAL)
+    @Severity(SeverityLevel.CRITICAL)
+    @Step("Fill practice form")
     @Description("Fill practice form")
     private void FillForm(){
         PracticeFormPage.TextBox_FirstName(driver).sendKeys("Lasha");
@@ -37,5 +47,15 @@ public class POM_Allure {
         PracticeFormPage.radio_gender(driver).click();
         PracticeFormPage.TextBox_Mobile(driver).sendKeys("1234567890");
         PracticeFormPage.Button_Submit(driver).click();
+    }
+
+    @Test(priority = 2)
+    @Severity(SeverityLevel.NORMAL)
+    @Step("Wait for success dialog and checks submitted data")
+    @Description("Waits for success dialog and checks submitted data")
+    private void CheckResults(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1));
+        wait.until(ExpectedConditions.presenceOfElementLocated(PracticeFormPage.Dialog_Success_Text_Locator()));
+
     }
 }
